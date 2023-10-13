8eaa8b409b7425105fe3ee69564c78b7c1ca527a
esty2022 <esther.duntoye@gmail.com>
2023-10-13 15:23:41 +0000

    add

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -1,3 +1,6 @@
+-- Creates a table users with below attributes
+
+
 CREATE TABLE IF NOT EXISTS users (
        id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
        email VARCHAR(255) NOT NULL UNIQUE,
